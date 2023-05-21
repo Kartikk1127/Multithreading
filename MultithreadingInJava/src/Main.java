30b1eea884289edd02a71d56bb359f7aeba76f53
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-21 12:56:19 +0530

    synchronization added

--- MultithreadingInJava/src/Main.java
+++ MultithreadingInJava/src/Main.java
@@ -1,3 +1,4 @@
+import com.Kartikey.Stack;
 import com.Kartikey.Thread1;
 import com.Kartikey.Thread2;
 
@@ -6,15 +7,29 @@ public class Main {
 
         System.out.println("Main is starting");
 
-        Thread thread1 = new Thread1("thread1");
-        thread1.setDaemon(false); //now this is a user thread
-        thread1.start();
+//        Thread thread1 = new Thread1("thread1");
+//        thread1.setDaemon(false); //now this is a user thread
+//        thread1.start();
+//
+//        Thread thread2 = new Thread(()->{
+//            for(int i=0;i<5;i++)
+//              System.out.println(Thread.currentThread() + ", "+i);
+//        }, " thread2");
+//        thread2.start();
 
-        Thread thread2 = new Thread(()->{
-            for(int i=0;i<5;i++)
-              System.out.println(Thread.currentThread() + ", "+i);
-        }, " thread2");
-        thread2.start();
+        Stack stack = new Stack(5);
+
+        new Thread(()->{
+            int counter=0;
+            while(++counter<10)
+                System.out.println("Pushed " + stack.push(100));
+        }, "Pusher").start();
+
+        new Thread(()->{
+            int counter=0;
+            while(++counter<10)
+                System.out.println("Popped " + stack.pop());
+        }, "Popper").start();
 
         System.out.println("Main is exiting");
 
