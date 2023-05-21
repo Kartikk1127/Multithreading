693d442f90bd46b17a816049202865e8ec2246e2
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-21 14:02:02 +0530

    thread states

--- MultithreadingInJava/src/Main.java
+++ MultithreadingInJava/src/Main.java
@@ -17,21 +17,38 @@ public class Main {
 //        }, " thread2");
 //        thread2.start();
 
-        Stack stack = new Stack(5);
+//        Stack stack = new Stack(5);
+//
+//        new Thread(()->{
+//            int counter=0;
+//            while(++counter<10)
+//                System.out.println("Pushed " + stack.push(100));
+//        }, "Pusher").start();
+//
+//        new Thread(()->{
+//            int counter=0;
+//            while(++counter<10)
+//                System.out.println("Popped " + stack.pop());
+//        }, "Popper").start();
+//
+//        System.out.println("Main is exiting")
 
-        new Thread(()->{
-            int counter=0;
-            while(++counter<10)
-                System.out.println("Pushed " + stack.push(100));
-        }, "Pusher").start();
+        Thread thread = new Thread(()->{
+            try{
+                Thread.sleep(1);
+                for(int i=10000;i>0;i--);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }, "States");
 
-        new Thread(()->{
-            int counter=0;
-            while(++counter<10)
-                System.out.println("Popped " + stack.pop());
-        }, "Popper").start();
+        thread.start();
 
-        System.out.println("Main is exiting");
+        while (true){
+            Thread.State state = thread.getState();
+            System.out.println(state);
+            if(state== Thread.State.TERMINATED) break;
+        }
 
     }
 }
